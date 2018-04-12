dd9b84937c09f7df1a095bc8d9a431c9469da84d
Phil Forrence <prf8@pitt.edu>
2018-04-12 11:47:30 -0400

    3 and 4

---
+++ prf8-nep36-assignment5/tests/Milestone4.java
@@ -0,0 +1,19 @@
+class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Test2().Start(0));
+
+    }
+
+}
+
+class Test2 {
+
+    public int Start(int y) {
+
+        //Arbitrary straight-line code
+
+        return y;
+
+    }
