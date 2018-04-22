2ab7a15fe3fd88c55751de7ffa61897f138c786a
nep36 <nep36@pitt.edu>
2018-04-22 16:29:25 -0400

    Milestone 5 added

---
+++ prf8-nep36-assignment5/tests/Milestone5.java
@@ -0,0 +1,20 @@
+class Test {
+
+    public static void main(String[] args) {
+
+        int i = 9;
+        if(i == 9) {
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
+        }
+        
+        int count = 0;
+        while(count < i) {
+            count++;
+            System.out.println(count);
+        }
+
+    }
+
+}
