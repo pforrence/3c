c31b1437a4687cf9d232559e5f692937d785d9db
nep36 <nep36@pitt.edu>
2018-04-22 21:00:55 -0400

    updated milestone 4

--- prf8-nep36-assignment5/tests/Milestone4.java
+++ prf8-nep36-assignment5/tests/Milestone4.java
@@ -13,8 +13,26 @@ class Test2 {
     public int Start(int y) {
 
         //Arbitrary straight-line code
-
-        return y;
+        int a;
+        int b;
+        
+        boolean b1;
+        boolean b2;
+        
+        a = 3;
+        a = a + a;
+        a = a + 1;
+        a = 1 + a;
+        a = a - 1 - 1;
+        a = 1 - a;
+        a = a * a;
+        b = 3 * 3;
+        
+        b1 = true;
+        b2 = false;
+        b1 = b1 && b2;
+        
+        return a;
 
     }
 }
